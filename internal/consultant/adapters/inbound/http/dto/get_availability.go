f7e0a9c6d0cb523529ca4b5c86e0f2bacc0cfe46
AppeiYA <appeimisic@gmail.com>
2026-08-23 23:29:11 +0100

    feat(availability): update GetAvailabilityResponse to use string for DayOfWeek

--- internal/consultant/adapters/inbound/http/dto/get_availability.go
+++ internal/consultant/adapters/inbound/http/dto/get_availability.go
@@ -3,14 +3,14 @@ package dto
 import "github.com/AppeiYA/consultation-platform/internal/consultant/domain"
 
 type GetAvailabilityResponse struct {
-	DayOfWeek int `json:"day_of_week"`
+	DayOfWeek string `json:"day_of_week"`
 	StartTime string `json:"start_time"`
 	EndTime   string `json:"end_time"`
 }
 
 func AvailabilityFromDomain(availability *domain.ConsultantAvailability) *GetAvailabilityResponse {
 	return &GetAvailabilityResponse{
-		DayOfWeek: int(availability.DayOfWeek()),
+		DayOfWeek: availability.DayOfWeek().String(),
 		StartTime: availability.StartTime().String(),
 		EndTime:   availability.EndTime().String(),
 	}
